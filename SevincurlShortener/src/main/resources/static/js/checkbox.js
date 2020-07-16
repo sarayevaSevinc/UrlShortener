fb127716e7da56f7390cd0a8000c56289fb2307e
Sevinc_Saraeva <CompEng*18>
2020-07-16 14:37:28 +0400

    first

--- SevincurlShortener/src/main/resources/static/js/checkbox.js
+++
@@ -1,3 +0,0 @@
-document.querySelector(".login-section-container-options-old-checkbox").addEventListener("click", function (evt) {
-  document.querySelector(".login-section-container-options-checkbox-wrapper").classList.toggle("active-label")
-});
